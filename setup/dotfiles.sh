0c43d72bd1c2407d4ab64d48fba5cad51ee1ec91
Zdeněk Foltýn <znk.fltn@gmail.com>
2026-03-10 20:35:55 +0100

    Added kitty config

--- setup/dotfiles.sh
+++ setup/dotfiles.sh
@@ -6,4 +6,8 @@ echo "Linking nvim config..."
 mkdir -p "${HOME}/.config/"
 ln -sf "${FRESH_SH_DIR}/dotfiles/nvim" "${HOME}/.config/nvim"
 
+echo "Linking kitty config..."
+mkdir -p "${HOME}/.config/"
+ln -sf "${FRESH_SH_DIR}/dotfiles/kitty" "${HOME}/.config/kitty"
+
 echo "Done!"
